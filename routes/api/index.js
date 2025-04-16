38d623a835b3fa6c96eac61cb9a294e5cfa4d916
Peter Knipp <peteraknipp@gmail.com>
2025-04-16 17:34:38 -0400

    Send url frag to json

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,20 @@
 const router = require('express').Router();
-const routes = ['only'];
-routes.forEach(route => router.use(`/${route}`, require(`./${route}`)));
+// const routes = ['only'];
+// routes.forEach(route => router.use(`/${route}`, require(`./${route}`)));
+
+router.get('/evaluate-expression/:exprStr', (req, res) => {
+    let exprStr = req.params.exprStr;
+    res.json({message: exprStr});
+});
+
+// router.get('/:rNmaxNmin', (req, res) => {
+    // let params = req.params.rNmaxNmin.split("-");
+    // let [error, xs] = parseParams(params);
+    // if (error) {
+    //   res.status(500);
+    //   res.json({error});
+    // } else {
+    //   res.json({message: JSON.stringify(xs)});
+    // }
+//   });
 module.exports = router;
